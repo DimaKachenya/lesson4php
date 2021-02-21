10a804c2686685ee49b378536ad6f549c2b3b853
Dima <53133762+DimaKachenya@users.noreply.github.com>
2021-02-21 11:43:37 +0300

    Update autorizationAction.php

--- autorizationAction.php
+++ autorizationAction.php
@@ -6,7 +6,7 @@ $data=$_POST;
 if(DBconnection::haveUser($data['email'],$data['password'])){
     $_SESSION['email']=$data['email'];
     $_SESSION['password']=md5($data['password']);
-    echo('<a href="autorization.php">К глвной</a> ');
+    echo('<a href="view.php">К глвной</a> ');
    // header('https://autorizationform.herokuapp.com/view.php');
 } else {
     echo "No user!";
